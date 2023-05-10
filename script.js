9bb993f9fe1adb0b623af32bbe712e139334f014
Kapil Sahu <116225142+SahuKapil@users.noreply.github.com>
2023-05-11 01:29:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
 
+// script.js
+let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'Pink Floyd', 'The Beatles', 'Aerosmith', 'The Who'];
+
+function stripArticles(name) {
+  return name.replace(/^(a |an |the )/i, '').trim();
+}
+
+bandNames.sort((a, b) => stripArticles(a) > stripArticles(b) ? 1 : -1);
+
+const bandList = document.getElementById('band');
+for (let i = 0; i < bandNames.length; i++) {
+  const li = document.createElement('li');
+  li.textContent = bandNames[i];
+  bandList.appendChild(li);
+}
