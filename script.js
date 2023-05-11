716d672a300b451a79aa0d9a5712044f967fb498
Kapil Sahu <116225142+SahuKapil@users.noreply.github.com>
2023-05-12 03:29:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-// Given array of band names
-let bandNames = ["The Beatles", "Anathema", "Aerosmith", "The Who", "Pink Floyd"];
+// The initial array with place names
+let arr = ['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
 
-// Remove articles from band names
-let updatedBandNames = bandNames.map((name) => {
-  return name.replace(/^(a |an |the )/i, "").trim();
+// The map function is used to create a new array of place names after filtering out the articles "a", "an" and "the"
+let updatedList = arr.map((element) => {
+	// Split the place name into words
+	let words = element.split(" ") ;
+
+	// Use the reduce function to iterate through each word of the place name and filter out articles
+	let updatedString = words.reduce((prev, current, index) => {
+		if(current != "The" && current != "an" && current != "a"){
+			// If the word is not an article, add it to the updated string
+			return prev + " " + current ;
+		}
+		// Otherwise, return the previous string
+		return prev ;
+	}, "")
+
+	// Return the updated string for this place name
+	return updatedString ;
+}) ;
+
+// Create a new object that maps each updated place name to its original place name
+let mp = {} ;
+updatedList.forEach( (element, index) => {
+	mp[element] = arr[index] ;
 });
 
-// Sort band names in lexicographic order
-updatedBandNames.sort();
+// Sort the updated place names alphabetically
+updatedList.sort(); 
 
-// Get the ul tag with id 'band'
-let bandList = document.querySelector("#band");
+// Create a final array of place names by looking up the original place names using the mapping object
+let finalAns = updatedList.map((element) => {
+    return mp[element] ;
+})
 
-// Append li tags for each band name
-updatedBandNames.forEach((name) => {
-  let li = document.createElement("li");
-  li.innerText = name;
-  bandList.appendChild(li);
-});
+// Output the final array of place names
+console.log(finalAns); 
