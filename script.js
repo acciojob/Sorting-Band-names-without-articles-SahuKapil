509f19a225612f695056cf83505d20af67f02001
Kapil Sahu <116225142+SahuKapil@users.noreply.github.com>
2023-05-12 02:07:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
+// The initial array with place names
 let arr = ['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
+
+// The map function is used to create a new array of place names after filtering out the articles "a", "an" and "the"
 let updatedList = arr.map((element) => {
-	// ["a", "some", "one"]
+	// Split the place name into words
 	let words = element.split(" ") ;
+
+	// Use the reduce function to iterate through each word of the place name and filter out articles
 	let updatedString = words.reduce((prev, current, index) => {
 		if(current != "The" && current != "an" && current != "a"){
+			// If the word is not an article, add it to the updated string
 			return prev + " " + current ;
 		}
+		// Otherwise, return the previous string
 		return prev ;
 	}, "")
 
+	// Return the updated string for this place name
 	return updatedString ;
 }) ;
-let mp = {} ; // {"some one" : "a some one"}
+
+// Create a new object that maps each updated place name to its original place name
+let mp = {} ;
 updatedList.forEach( (element, index) => {
 	mp[element] = arr[index] ;
 });
+
+// Sort the updated place names alphabetically
 updatedList.sort(); 
 
+// Create a final array of place names by looking up the original place names using the mapping object
 let finalAns = updatedList.map((element) => {
     return mp[element] ;
 })
 
+// Output the final array of place names
 console.log(finalAns); 
