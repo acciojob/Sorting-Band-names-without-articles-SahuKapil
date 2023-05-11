fec8792b5c51fd2569a8323bbfa00eed23333a1b
Kapil Sahu <116225142+SahuKapil@users.noreply.github.com>
2023-05-12 02:11:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,22 @@
-// The initial array with place names
-let arr = ['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
+() => {
+  cy.visit('https://example.com');
+  
+  // Wait for the li elements to appear
+  cy.get('li', { timeout: 10000 }).should('have.length.at.least', 1);
 
-// The map function is used to create a new array of place names after filtering out the articles "a", "an" and "the"
-let updatedList = arr.map((element) => {
-	// Split the place name into words
-	let words = element.split(" ") ;
+  let tags_content = [];
+  const li_tags_count = document.getElementsByTagName('li').length;
 
-	// Use the reduce function to iterate through each word of the place name and filter out articles
-	let updatedString = words.reduce((prev, current, index) => {
-		if(current != "The" && current != "an" && current != "a"){
-			// If the word is not an article, add it to the updated string
-			return prev + " " + current ;
-		}
-		// Otherwise, return the previous string
-		return prev ;
-	}, "")
+  // Getting li tags content and checking if they are without any articles and in sorted order
+  for (let index = 0; index < li_tags_count; index++) {
+    cy.get('li').eq(index).then($el => {
+      tags_content.push($el.text());
+    });
+  }
 
-	// Return the updated string for this place name
-	return updatedString ;
-}) ;
-
-// Create a new object that maps each updated place name to its original place name
-let mp = {} ;
-updatedList.forEach( (element, index) => {
-	mp[element] = arr[index] ;
-});
-
-// Sort the updated place names alphabetically
-updatedList.sort(); 
-
-// Create a final array of place names by looking up the original place names using the mapping object
-let finalAns = updatedList.map((element) => {
-    return mp[element] ;
-})
-
-// Output the final array of place names
-console.log(finalAns); 
+  tags_content.forEach(tag_content => {
+    const first_word = tag_content.split(' ')[0];
+    let not_article = first_word != 'A' && first_word != 'An' && first_word != 'The';
+    expect(not_article).to.be.equal(true);
+  });
+}
